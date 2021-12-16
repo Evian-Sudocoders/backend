738898cc58a99e1d05c292fbb3e00d69f64e5931
HarshilMendpara <mendparaharshil@gmail.com>
2021-12-16 12:46:05 +0530

    Initialized razorpay

---
+++ server/common/razorpay.js
@@ -0,0 +1,8 @@
+import Razorpay from 'razorpay';
+
+var razorpayInstance = new Razorpay({
+  key_id: process.env.RAZORPAY_KEY_ID,
+  key_secret: process.env.RAZORPAY_KEY_SECRET,
+});
+
+export default razorpayInstance;
